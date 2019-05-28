c8c3b93efd9a7ea9ccd8730e3defdb2027577674
ganaram <guillermocr98@gmail.com>
2019-05-28 17:30:23 +0200

    Corregidos errores en registro.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,8 +19,6 @@ $factory->define(App\User::class, function (Faker $faker) {
     $name = $faker->name;
     return [
         'name' => $name,
-        'slug' => str_slug($name,'-'),
-        'phone'=> $faker->phoneNumber,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
